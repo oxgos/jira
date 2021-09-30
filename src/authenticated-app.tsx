56c3eeda86cee1ee886971fc4942796ea26728c3
jianwei.guo <jianwei.guo@rootcloud.com>
2021-09-30 16:20:39 +0800

    feat: 添加kanban和epic路由

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -9,6 +9,7 @@ import { Dropdown, Menu, Button } from 'antd'
 import { Navigate, Route, Routes } from 'react-router'
 import { BrowserRouter as Router } from 'react-router-dom'
 import ProjectScreen from 'screens/project'
+import { resetRoute } from 'hooks/common'
 
 export const AuthenticatedApp = () => {
   return (
@@ -36,7 +37,9 @@ const PageHeader = () => {
   return (
     <Header between={true}>
       <HeaderLeft gap={true}>
-        <SoftwareLogo width={'18rem'} color={'rgb(38,132,255)'} />
+        <Button type={'link'} onClick={resetRoute}>
+          <SoftwareLogo width={'18rem'} color={'rgb(38,132,255)'} />
+        </Button>
         <h2>项目</h2>
         <h2>用户</h2>
       </HeaderLeft>
--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,5 +1,22 @@
+import { Navigate, Route, Routes } from 'react-router'
+import { Link } from 'react-router-dom'
+import { EpicScreen } from './epic'
+import { KanbanScreen } from './kanban'
+
 const ProjectScreen = () => {
-  return <div>{'ProjectScreen'}</div>
+  return (
+    <div>
+      <h1>{'ProjectScreen'}</h1>
+      {/* to的地址加上'/'，就变成根路由了 */}
+      <Link to={'kanban'}>看板</Link>
+      <Link to={'epic'}>任务组</Link>
+      <Routes>
+        <Route path={'/kanban'} element={<KanbanScreen />} />
+        <Route path={'/epic'} element={<EpicScreen />} />
+        <Navigate to={window.location.pathname + '/kanban'} />
+      </Routes>
+    </div>
+  )
 }
 
 export default ProjectScreen
