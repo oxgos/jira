df3a749f6e7ea84e536a6eb6c3e386184bd31369
jianwei.guo <jianwei.guo@rootcloud.com>
2021-11-02 17:05:45 +0800

    feat: 利用useReducer优化useAsync

--- src/hooks/use-async.ts
+++ src/hooks/use-async.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useReducer, useCallback } from 'react'
 import { useMountedRef } from './common'
 
 interface State<D> {
@@ -17,20 +17,33 @@ const defaultConfig = {
   throwOnError: false // 判断是否使用useAsync的error状态
 }
 
+const useSafeDispatch = <T>(dispatch: (...args: T[]) => void) => {
+  const mountedRef = useMountedRef()
+  return useCallback(
+    (...args: T[]) => {
+      return mountedRef.current ? dispatch(...args) : void 0
+    },
+    [dispatch, mountedRef]
+  )
+}
+
 export const useAsync = <D>(
   initialState?: State<D>,
   initialConfig?: typeof defaultConfig
 ) => {
   const config = { ...defaultConfig, ...initialConfig }
-  const [state, setState] = useState<State<D>>({
-    ...defaultInitialState,
-    ...initialState
-  })
+  const [state, dispatch] = useReducer(
+    (state: State<D>, action: Partial<State<D>>) => ({ ...state, ...action }),
+    {
+      ...defaultInitialState,
+      ...initialState
+    }
+  )
   // useState直接传入函数的含义是：惰性初始化：所以，要用useState保存函数，不能直接传入函数
   // useState: https://codesandbox.io/s/li-yong-usestatebao-cun-han-shu-ob72k
   // useRef: https://codesandbox.io/s/li-yong-userefbao-cun-han-shu-pktr3
   const [retry, setRetry] = useState(() => () => {})
-  const mountedRef = useMountedRef()
+  const safeDispatch = useSafeDispatch(dispatch)
   // 可以用useRef代替实现retry
   // const ref = useRef()
   // const retry = () => {
@@ -40,22 +53,22 @@ export const useAsync = <D>(
   // }
   const setData = useCallback(
     (data: D) =>
-      setState({
+      safeDispatch({
         data,
         stat: 'success',
         error: null
       }),
-    []
+    [safeDispatch]
   )
 
   const setError = useCallback(
     (error: Error) =>
-      setState({
+      safeDispatch({
         data: null,
         stat: 'error',
         error
       }),
-    []
+    [safeDispatch]
   )
 
   const run = useCallback(
@@ -74,19 +87,15 @@ export const useAsync = <D>(
       //   ...state,
       //   stat: 'loading'
       // })
-      setState((prevState) => ({ ...prevState, stat: 'loading' }))
+      safeDispatch({ stat: 'loading' })
       return promise
         .then((data) => {
-          if (mountedRef.current) {
-            setData(data)
-          }
+          setData(data)
           return data
         })
         .catch((error) => {
           // catch会消化异常，如果不主动抛出，外面是接收不到异常的
-          if (mountedRef.current) {
-            setError(error)
-          }
+          setError(error)
           if (config.throwOnError) return Promise.reject(error)
           return error
         })
@@ -94,7 +103,7 @@ export const useAsync = <D>(
     // 这里依赖了state，也会触发无限循环，因为上面有setState,state一改变又重新调用run
     // 解决: 利用setState(prevState => {}),prevState代表当前state，所以就不需要再依赖state
     // [config.throwOnError, mountedRef, setData, setError, state]
-    [config.throwOnError, mountedRef, setData, setError]
+    [config.throwOnError, setData, setError, safeDispatch]
   )
   return {
     isIdle: state.stat === 'idle',
